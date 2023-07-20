a7cc8ab91da68ea47ccff1e83b173ce94caebd02
ahmedfares_sh <shahhhen10@gmail.com>
2023-07-20 13:10:48 +0300

    update 1

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-// js of header 
+// js of header
 
 const megaMenuBtn = document.getElementById("mega");
 const megaMenuDiv = document.getElementById("mega-menu");
+
+const skillSection = document.getElementById("skill");
+const progressSpans = document.querySelectorAll(".bar span");
+
+window.onscroll = function () {
+   console.log(window.scrollY);
+   console.log(skillSection.offsetTop);
+  if (window.scrollY >= skillSection.offsetTop - 300) {
+
+   
+    for(let i =0;i<progressSpans.length;i++){
+        progressSpans[i].style.width = progressSpans[i].dataset.width
+    }
+  }
+};
 megaMenuBtn.onclick = function () {
   megaMenuDiv.classList.toggle("active-menu");
 };
